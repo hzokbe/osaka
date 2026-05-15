0aa8246ebcdf50528330929b51b608eefd95fa63
hzokbe <hzokbe@gmail.com>
2026-05-15 18:49:14 -0300

    feat(item): add Osaka item group

--- src/main/kotlin/com/hzokbe/osaka/Osaka.kt
+++ src/main/kotlin/com/hzokbe/osaka/Osaka.kt
@@ -1,14 +1,19 @@
 package com.hzokbe.osaka
 
+import com.hzokbe.osaka.item.ModItemGroups
 import com.hzokbe.osaka.item.ModItems
 import net.fabricmc.api.ModInitializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 object Osaka : ModInitializer {
-	const val MOD_ID = "osaka"
+    const val MOD_ID = "osaka"
 
     val logger: Logger = LoggerFactory.getLogger(MOD_ID)
 
-	override fun onInitialize() = ModItems.register()
+    override fun onInitialize() {
+        ModItems.register()
+
+        ModItemGroups.register()
+    }
 }
